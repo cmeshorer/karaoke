8671febe105f431ce44850474cf9b8b26add6e77
cmeshorer <cmeshorer@gmail.com>
2024-10-14 11:08:49 +0200

    chore: add theme/constants

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const constants = {
+  elements: {
+    animation: 400,
+    avatar: 56,
+    infoContainer: 560,
+    loader: 60,
+  },
+  radius: {
+    normal: 20,
+  },
+  size: {
+    normal: 20,
+    large: 60,
+  },
+  spacing: {
+    tiny: 5,
+    small: 10,
+    normal: 15,
+    large: 20,
+    big: 40,
+  },
+};
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  purple: "#af7acc",
+  green: "#dff7e3",
+  orange: "#ffac69",
+  beige: "#f7f0ed",
+  blue: {
+    regular: "#6573db",
+    pale: "#ebf2f5",
+  },
+  greyscale: {
+    dark: "#000000",
+    regular: "#8f8888",
+    pale: "#929596",
+    white: "#ffffff",
+  },
+  status: {
+    success: "#68965a",
+    warning: "#ffac69",
+    error: "#eb4034",
+  },
+};
